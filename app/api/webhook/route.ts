9c4ad607b0e7ae29b98c1d4bf79d8206c5323e31
shogani1 <sashalink14@gmail.com>
2024-01-12 13:13:36 +0200

    fixed bugs

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,13 +1,16 @@
 import Stripe from "stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 
 export async function POST(req: Request) {
   const body = await req.text();
   const signature = headers().get("Stripe-Signature") as string;
+
   let event: Stripe.Event;
+
   try {
     event = stripe.webhooks.constructEvent(
       body,
@@ -15,16 +18,20 @@ export async function POST(req: Request) {
       process.env.STRIPE_WEBHOOK_SECRET!
     );
   } catch (error) {
-    return new NextResponse("Webhook Error", { status: 400 });
+    return new NextResponse("Webhook error", { status: 400 });
   }
+
   const session = event.data.object as Stripe.Checkout.Session;
+
   if (event.type === "checkout.session.completed") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
-    if (!session.metadata?.orgId) {
+
+    if (!session?.metadata?.orgId) {
       return new NextResponse("Org ID is required", { status: 400 });
     }
+
     await db.orgSubscription.create({
       data: {
         orgId: session?.metadata?.orgId,
@@ -37,10 +44,12 @@ export async function POST(req: Request) {
       },
     });
   }
+
   if (event.type === "invoice.payment_succeeded") {
     const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
+
     await db.orgSubscription.update({
       where: {
         stripeSubscriptionId: subscription.id,
@@ -53,5 +62,6 @@ export async function POST(req: Request) {
       },
     });
   }
+
   return new NextResponse(null, { status: 200 });
 }
